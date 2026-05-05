e65a7a2230b60067a83095e8dc8563f85cee0cd4
t2ytanaka-max <t2y.tanaka@gmail.com>
2026-05-05 18:27:17 +0900

    Update firebase config with newly created database

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Firebase Configuration (Replace with your own)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "search-omura.firebaseapp.com",
-  projectId: "search-omura",
-  storageBucket: "search-omura.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDdOW3-INeDWRm0XKDGtn7pji_fXV2wQS0",
+  authDomain: "search-omura-fire-corps.firebaseapp.com",
+  projectId: "search-omura-fire-corps",
+  storageBucket: "search-omura-fire-corps.firebasestorage.app",
+  messagingSenderId: "94083214990",
+  appId: "1:94083214990:web:5c11174b047392724c23df",
 };
 
 const app = initializeApp(firebaseConfig);
